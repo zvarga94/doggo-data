625bf3b22f6016b173f05950f04e82ce07cb25fd
zvarga <zsolt@korro.ai>
2025-01-16 20:27:31 +0100

    Fixing downloading images. Minimal clode clean up.

--- src/scrape.py
+++ src/scrape.py
@@ -10,33 +10,29 @@ import hashlib
 
 import logging
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def download_images(container, path_out_dir):
-    # Locate the parent container
-
-    # Find all child div elements within the container
     all_divs = container.find_elements(By.XPATH, "./div")
 
     image_urls = []
 
-    # Loop through each div and download images
     for div_index, div in enumerate(all_divs):
         try:
             images = div.find_elements(By.TAG_NAME, "img")
             for img_index, image in enumerate(images):
                 try:
                     src = image.get_attribute("src")
-                    image_urls.append(image_urls)
-                    if src and src not in image_urls:  # If src is not None
-                        response = requests.get(src)  # Download the image
+                    if src and src not in image_urls:
+                        response = requests.get(src)
                         if response.status_code == 200:
-                            # Save the image with a unique name
                             path_img = path_out_dir / f"{div_index}_{img_index}.jpg"
                             with path_img.open("wb") as file:
                                 file.write(response.content)
                             logger.info(f"Downloaded: {src}")
+                            image_urls.append(src)
                 except Exception as e:
                     logger.warning(f"Failed to download image in div[{div_index}]: {e}")
         except Exception as e:
@@ -45,22 +41,22 @@ def download_images(container, path_out_dir):
 
 
 def scrape_one_page(driver, href: str):
+    logger.info(f"Scraping {href}")
+
     uid = hashlib.md5(base_url.encode()).hexdigest()[0:8]
 
     driver.get(href)
-    # time.sleep(3)
+    time.sleep(3)
 
     xpaths = {
-        "data": '//*[@id="bootstrap-panel"]/div[2]',  # data
-        "description": '//*[@id="bootstrap-panel--2"]/div[2]',  # description
-        "trait": '//*[@id="bootstrap-panel--3"]/div[2]',  # traits
-        "behavior": '//*[@id="bootstrap-panel--4"]/div[2]',  # behaviour
-        "table": '//*[@id="block-views-block-workflow-block-1"]/div/div/div/div',  # table
+        "data": '//*[@id="bootstrap-panel"]/div[2]',
+        "description": '//*[@id="bootstrap-panel--2"]/div[2]',
+        "trait": '//*[@id="bootstrap-panel--3"]/div[2]',
+        "behavior": '//*[@id="bootstrap-panel--4"]/div[2]',
+        "table": '//*[@id="block-views-block-workflow-block-1"]/div/div/div/div',
     }
     d = {k: driver.find_element(By.XPATH, v).text for k, v in xpaths.items()}
-    d
 
-    # Create a directory to save images
     path_out_dir = Path(f"slider_images/{uid}")
 
     path_out_dir.mkdir(parents=True, exist_ok=True)
@@ -71,13 +67,13 @@ def scrape_one_page(driver, href: str):
     )
 
     download_images(parent_container, path_out_dir)
-    print()
+    return d
 
 
 if __name__ == "__main__":
 
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=options)
@@ -85,18 +81,15 @@ if __name__ == "__main__":
     page_indices = range(0, 100)
     base_url = "https://www.tappancs.hu/gazdit-keresunk/archivum/orokbefogadott-tappancsos-kutyak?page="
 
-    # per dog
-
-    base_url = "https://www.tappancs.hu/gazdit-keresunk/archivum/orokbefogadott-tappancsos-kutyak/kali"
-
-    scrape_one_page(driver, base_url)
-
-    # generate uid from base url
+    l_d = []
 
     for page_index in page_indices:
+        logger.info(f"Scraping page {page_index}")
+
         url = base_url + str(page_index)
 
         driver.get(url)
+        time.sleep(3)
 
         links = driver.find_elements(
             By.XPATH, '//*[@id="block-views-block-tappancs-dogs-block-5"]//a'
@@ -111,10 +104,8 @@ if __name__ == "__main__":
         links_dog = list(set(links_dog))
 
         for link in links_dog:
-            driver.get(link)
-
-            bla = '//*[@id="bootstrap-panel"]/div[2]'
-            bla2 = driver.find_element(By.XPATH, bla)
-            bla2.get_attribute("href")
-
+            try:
+                l_d.append(scrape_one_page(driver, link))
+            except Exception as e:
+                logger.warning(f"Failed to scrape {link}: {e}")
             time.sleep(3)
